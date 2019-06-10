bf507e0e41dc64941c1e77b0155794de6ac4ec82
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-10 00:04:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,16 @@
 def oxford_comma(array)
+  if array.length = 1 
+    return array 
+  elsif array.length = 2 
   array.join(" and ")
+elsif array.length = 3 
+
   
 
 end
+
+
+# if the array = 1 then return it 
+# if the array = 2 add and between them
+# if the array = 3 add commas/ and appropriately 
+# if more than 3 = appropriately puncuate it. length-1 place the and 
